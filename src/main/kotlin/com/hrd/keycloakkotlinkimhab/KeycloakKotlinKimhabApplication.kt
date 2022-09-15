a51131aee61019f2e4df6b4ee29d6f51cb8d94b9
kimhab <69948342+kimhab11@users.noreply.github.com>
2022-09-15 19:02:47 +0700

    Initial commit

---
+++ src/main/kotlin/com/hrd/keycloakkotlinkimhab/KeycloakKotlinKimhabApplication.kt
@@ -0,0 +1,11 @@
+package com.hrd.keycloakkotlinkimhab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeycloakKotlinKimhabApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeycloakKotlinKimhabApplication>(*args)
+}
